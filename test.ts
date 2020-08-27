33ea74f14ba27f218b8aebae8cba384a043b0af7
tom <tom@qq.com>
2020-08-27 17:03:42 +0700

    Revert "function1"
    This reverts commit 6c306ad926cd772ec5049aacee0ef13ce581153a.
    
    # Conflicts:
    #	test.ts

--- test.ts
+++ test.ts
@@ -1,13 +1,1 @@
-void base_func() {
-    printf("this is a crash %d\n", *p);111
-
-    printf("branch func1\n");}
-}
-
-void dev_func_1() {
-    printf("dev func 1\n");
-}
-
-void dev_func_2() {
-    printf("dev func 1\n");
-}
+function1
