51e052db73df454d00209d50e202415f93e9a09d
tom <tom@qq.com>
2020-08-27 16:48:10 +0700

    Revert "branch_func_1"
    This reverts commit 707f43a7ce96b95023a1227d255d1d4bfba8febf.
    
    # Conflicts:
    #	test.ts

--- test.ts
+++ test.ts
@@ -1,11 +1,3 @@
 void base_func() {
     printf("this is a crash %d\n", *p);111
-}
-
-void dev_func_1() {
-    printf("dev func 1\n");
-}
-
-void branch_func_1(){
-    printf("branch func1\n");
-}
+    printf("branch func1\n");}
